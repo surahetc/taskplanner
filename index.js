e59105349e61757d6a25c0f17696b005e22240c6
Suraj Mourya <suraj.mourya@blrsimplilearn.com>
2021-08-26 10:52:03 +1100

    version 2.0

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 
 const fs = require('fs');
-  
-
 const args = process.argv;
-  
-
 const currentWorkingDirectory = args[1].slice(0, -8);
 
 if (fs.existsSync(currentWorkingDirectory + 'todo.txt') === false) {
     let createStream = fs.createWriteStream('todo.txt');
     createStream.end();
   }
-  if (fs.existsSync(currentWorkingDirectory + 'done.txt') === false) {
-    let createStream = fs.createWriteStream('done.txt');
-    createStream.end();
-  }
+ 
 
-  const InfoFunction = () => {
-    const UsageText = `
-  Usage :-
-  $ node index.js add "todo item"  # Add a new todo
-  $ node index.js ls               # Show remaining todos
-  $ node index.js del NUMBER       # Delete a todo
-  $ node index.js done NUMBER      # Complete a todo
-  $ node index.js help             # Show usage
-  $ node index.js report           # Statistics`;
-    
-    console.log(UsageText);
-  };
+  
 
   const listFunction = () => {
   
@@ -149,167 +131,27 @@ if (fs.existsSync(currentWorkingDirectory + 'todo.txt') === false) {
   };
 
 
-  const doneFunction = () => {
   
-    
-    const doneIndex = args[3]; 
-      
-    
-    if (doneIndex) { 
-      
-      // Empty array
-      let data = []; 
-        
-      // Create a new date object
-      let dateobj = new Date(); 
-        
-      // Convert it to string and slice only the
-      // date part, removing the time part
-      let dateString = dateobj.toISOString().substring(0, 10); 
-        
-      // Read the data from todo.txt
-      const fileData = fs
-        .readFileSync(currentWorkingDirectory + 'todo.txt')
-        .toString(); 
-        
-      // Read the data from done.txt
-      const doneData = fs
-        .readFileSync(currentWorkingDirectory + 'done.txt')
-        .toString(); 
-          
-      // Split the todo.txt data
-      data = fileData.split('\n'); 
-        
-      // Filter for any empty lines
-      let filterData = data.filter(function (value) {
-        return value !== '';
-      }); 
-        
-      // If done index is greater than no. of task or <=0
-      if (doneIndex > filterData.length || doneIndex <= 0) {
-        console.log('Error: todo #' 
-            + doneIndex + ' does not exist.');
-      } else {
-    
-        // Delete the task from todo.txt
-        // data and store it
-        const deleted = filterData.splice(
-            filterData.length - doneIndex, 1);
-          
-        // Join the array to create a string
-        const newData = filterData.join('\n'); 
-          
-        // Write back the data in todo.txt
-        fs.writeFile(
-          currentWorkingDirectory + 'todo.txt',
-          newData, 
-          function (err) {
-            if (err) throw err;
-          },
-        );
-          
-        // Write the stored task in done.txt
-        // along with date string
-        fs.writeFile( 
-          currentWorkingDirectory + 'done.txt',
-          'x ' + dateString + ' ' + deleted 
-           + '\n' + doneData,
-          function (err) {
-            if (err) throw err;
-            console.log('Marked todo #' 
-              + doneIndex + ' as done.');
-          },
-        );
-      }
-    } else { 
-    
-      // If argument was not passed
-      console.log('Error: Missing NUMBER for'
-          + ' marking todo as done.');
-    }
-  };
-
-
-
-
-  const reportFunction = () => {
   
-    // Create empty array for data of todo.txt
-    let todoData = [];
-  
-    // Create empty array for data of done.txt
-    let doneData = [];
-  
-    // Create a new date object
-    let dateobj = new Date();
-  
-    // Slice the date part
-    let dateString = dateobj.toISOString().substring(0, 10);
-  
-    // Read data from both the files
-    const todo = fs.readFileSync(currentWorkingDirectory
-                + 'todo.txt').toString();
-    const done = fs.readFileSync(currentWorkingDirectory
-                + 'done.txt').toString();
-  
-    // Split the data from both files
-    todoData = todo.split('\n');
-  
-    doneData = done.split('\n');
-    let filterTodoData = todoData.filter(function(value) {
-        return value !== '';
-    });
-  
-    let filterDoneData = doneData.filter(function(value) {
-  
-        // Filter both the data for empty lines
-        return value !== '';
-    });
-  
-    console.log(
-        dateString +
-        ' ' +
-        'Pending : ' +
-        filterTodoData.length +
-        ' Completed : ' +
-        filterDoneData.length,
-        
-    );
-};
-
 switch (args[2]) {
     case 'add': {
         addFunction();
         break;
     }
     
-    case 'ls': {
+    case 'list': {
         listFunction();
         break;
     }
     
-    case 'del': {
+    case 'delete': {
         deleteFunction();
         break;
     }
         
-    case 'done': {
-        doneFunction();
-        break;
-    }
-        
-    case 'help': {
-        InfoFunction();
-        break;
-    }
-        
-    case 'report': {
-        reportFunction();
-        break;
-    }
         
     default: {
-        InfoFunction();
+        console.log("Wrong command line argument")
         
     }
     }
